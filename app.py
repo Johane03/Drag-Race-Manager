8f275d00bda442357d04f7f8b2a847c6b73c7c08
Johané Badenhorst <123386417+Johane03@users.noreply.github.com>
2025-08-30 04:04:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -557,4 +557,6 @@ def load_excel():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    host = '127.0.0.1' if debug else '0.0.0.0'
+    app.run(debug=debug, host=host, port=port)
